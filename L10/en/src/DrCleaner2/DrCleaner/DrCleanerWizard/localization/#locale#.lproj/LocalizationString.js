6d2de43992e40200d1d4eee305e3660ca7c7c474
ling_ma <ling_ma@trendmicro.com.cn>
2015-04-07 15:32:01 +0800

    add Dr.Cleanerv2.0 strings folder
    add Dr.Cleanerv2.0 strings folder

---
+++ L10/en/src/DrCleaner2/DrCleaner/DrCleanerWizard/localization/#locale#.lproj/LocalizationString.js
@@ -0,0 +1,20 @@
+(function() {
+
+    /////////////L10N string start////////////////
+    var ATLocalization = {
+        First_Guide_Title : "Smart Clean, Start Now",
+        First_Guide_Description : "The most popular free disk clean and memory optimize app.",
+    	Second_Guide_Title : "Smart Clean, Reclaim immediately",
+    	Second_Guide_Description : "Real-time monitor and reclaim unused memory when the app quits.",
+    	Third_Guide_Title : "Thorough Cleaning",
+    	Third_Guide_Description : "Remove the junk files of uninstalled app. Delete iTunes and iOS temporary files.",
+        Fourth_Guide_Setting_title : "Get much better experience",
+        Fourth_Guide_Setting_Auto_Start : "Auto-start when logging in to computer",
+        Fourth_Guide_Setting_Auto_Start_add : "(Recommend)",
+        Fourth_Guide_Enter_Button : "Start Now"
+    }
+    /////////////L10N string end//////////////////
+    var exports = window;
+
+    exports.ATLocalization = ATLocalization;
+})();
